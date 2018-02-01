54bbaa3ce2baa8ac78f78695bc81a15e1b22020a
Nebojša Kamber <kamber@nebojsa.in.rs>
2018-02-01 09:34:03 +0100

    Namespace by vendor

--- RequestBuilder.php
+++ RequestBuilder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests;
+namespace Nebkam;
 
 use Symfony\Bundle\FrameworkBundle\Client;
 
--- ResponseWrapper.php
+++ ResponseWrapper.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests;
+namespace Nebkam;
 
 use Symfony\Component\HttpFoundation\Response;
 
