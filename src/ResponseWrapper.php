cc7157ecaa335152a4a71f21366238800568ddb9
Nebojša Kamber <kamber@nebojsa.in.rs>
2018-02-01 12:47:17 +0100

    $response->isUnprocessable()

--- src/ResponseWrapper.php
+++ src/ResponseWrapper.php
@@ -28,11 +28,25 @@ class ResponseWrapper
 		return $this->response->getStatusCode() === Response::HTTP_CREATED;
 		}
 
+	/**
+	 * @return bool
+	 */
+	public function isUnprocessable(): bool
+		{
+		return $this->response->getStatusCode() === Response::HTTP_UNPROCESSABLE_ENTITY;
+		}
+
+	/**
+	 * @return bool
+	 */
 	public function isOk(): bool
 		{
 		return $this->response->isOk();
 		}
 
+	/**
+	 * @return bool
+	 */
 	public function isEmpty(): bool
 		{
 		return $this->response->isEmpty();
