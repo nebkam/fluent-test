23cd33341149eab344c650ce799ff8cf497fe4da
Nebojša Kamber <kamber@nebojsa.in.rs>
2018-02-01 09:12:09 +0100

    Initial commit

---
+++ RequestBuilder.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+
+class RequestBuilder
+	{
+	/**
+	 * @var Client
+	 */
+	private $client;
+
+	/**
+	 * @var string
+	 */
+	private $method;
+
+	/**
+	 * @var string
+	 */
+	private $uri;
+
+	/**
+	 * @var string
+	 */
+	private $content;
+
+	/**
+	 * @var array
+	 */
+	private $server = [];
+
+	/**
+	 * @param Client $client
+	 * @return RequestBuilder
+	 */
+	public function setClient(Client $client): RequestBuilder
+		{
+		$this->client = $client;
+
+		return $this;
+		}
+
+	/**
+	 * @param string $method
+	 * @return RequestBuilder
+	 */
+	public function setMethod(string $method): RequestBuilder
+		{
+		$this->method = $method;
+
+		return $this;
+		}
+
+	/**
+	 * @param string $uri
+	 * @return RequestBuilder
+	 */
+	public function setUri(string $uri): RequestBuilder
+		{
+		$this->uri = $uri;
+
+		return $this;
+		}
+
+	/**
+	 * @param string $content
+	 * @return RequestBuilder
+	 */
+	public function setContent(string $content): RequestBuilder
+		{
+		$this->content = $content;
+
+		return $this;
+		}
+
+	/**
+	 * @param mixed $content
+	 * @return RequestBuilder
+	 */
+	public function setJsonContent($content): RequestBuilder
+		{
+		$this->content = json_encode($content);
+
+		return $this;
+		}
+
+	/**
+	 * @return RequestBuilder
+	 */
+	public function sendAsAdmin(): RequestBuilder
+		{
+		$this->server = array_merge($this->server, [
+			'PHP_AUTH_USER' => 'admin',
+			'PHP_AUTH_PW'   => $this->client->getContainer()->getParameter('test_admin_pass')
+		]);
+
+		return $this;
+		}
+
+	/**
+	 * @return ResponseWrapper
+	 */
+	public function getResponse(): ResponseWrapper
+		{
+		$this->client->request($this->method, $this->uri, [], [], $this->server, $this->content);
+
+		return new ResponseWrapper($this->client->getResponse());
+		}
+
+	/**
+	 * @return RequestBuilder
+	 */
+	public static function create()
+		{
+		return new self();
+		}
+	}
---
+++ ResponseWrapper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseWrapper
+	{
+	/**
+	 * @var Response
+	 */
+	private $response;
+
+	/**
+	 * ResponseWrapper constructor.
+	 * @param Response $response
+	 */
+	public function __construct(Response $response)
+		{
+		$this->response = $response;
+		}
+
+	/**
+	 * @return bool
+	 */
+	public function isCreated(): bool
+		{
+		return $this->response->getStatusCode() === Response::HTTP_CREATED;
+		}
+
+	public function isOk(): bool
+		{
+		return $this->response->isOk();
+		}
+
+	public function isEmpty(): bool
+		{
+		return $this->response->isEmpty();
+		}
+
+	/**
+	 * @return mixed
+	 */
+	public function getJsonContent()
+		{
+		return json_decode($this->response->getContent(), true);
+		}
+	}
