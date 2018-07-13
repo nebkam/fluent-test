1c2379c13d4d1c68e33edc69f4540b58ff1ff0f2
Nebojša Kamber <kamber@nebojsa.in.rs>
2018-07-13 13:59:02 +0200

    Expose the whole response object for flexibility

--- src/ResponseWrapper.php
+++ src/ResponseWrapper.php
@@ -116,4 +116,12 @@ class ResponseWrapper
 		{
 		return $this->response->headers;
 		}
+
+	/**
+	 * @return Response
+	 */
+	public function getResponse(): Response
+		{
+		return $this->response;
+		}
 	}
